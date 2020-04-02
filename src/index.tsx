971eae3f6647433f315154e85cad796aad5c4b1f
Miikis <Miikis@users.noreply.github.com>
2020-04-02 16:45:13 -0700

    Setup emotion
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Global, css } from "@emotion/core"
+
+import App from "./App"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Global
+            styles={css`
+                body {
+                    margin: 0;
+                    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+                        "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans",
+                        "Droid Sans", "Helvetica Neue", sans-serif;
+                    -webkit-font-smoothing: antialiased;
+                    -moz-osx-font-smoothing: grayscale;
+                }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+                code {
+                    font-family: source-code-pro, Menlo, Monaco, Consolas,
+                        "Courier New", monospace;
+                }
+            `}
+        />
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
