6a7d5f9fdf1ec7799714bc19de47aa0b49170385
Miikis <Miikis@users.noreply.github.com>
2020-04-02 19:56:40 -0700

    Setup login page layout/styles
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom"
 import IndexPage from "./pages/index"
 import VolunteerPage from "./pages/volunteer"
 import VolunteerSuccessPage from "./pages/volunteer-success"
+import LoginPage from "./pages/login"
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Route exact path="/" component={IndexPage} />
             <Route path="/volunteer" component={VolunteerPage} />
             <Route path="/volunteer-success" component={VolunteerSuccessPage} />
+            <Route path="/login" component={LoginPage} />
         </Switch>
     )
 }
