16a5736a94b20d952c2ce66024402df603b5e1ba
Miikis <Miikis@users.noreply.github.com>
2020-04-02 17:28:14 -0700

    Setup basic layout w/ react helmet
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,16 @@
 import React from "react"
-import { css } from "@emotion/core"
+import { Helmet } from "react-helmet"
+
 import logo from "./logo.svg"
+
+import Layout from "./components/layout"
 
 function App() {
     return (
-        <div
-            css={css`
-                text-align: center;
-
-                .App-logo {
-                    height: 40vmin;
-                    pointer-events: none;
-                }
-
-                @media (prefers-reduced-motion: no-preference) {
-                    .App-logo {
-                        animation: App-logo-spin infinite 20s linear;
-                    }
-                }
-
-                .App-header {
-                    background-color: #282c34;
-                    min-height: 100vh;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    font-size: calc(10px + 2vmin);
-                    color: white;
-                }
-
-                .App-link {
-                    color: #61dafb;
-                }
-
-                @keyframes App-logo-spin {
-                    from {
-                        transform: rotate(0deg);
-                    }
-                    to {
-                        transform: rotate(360deg);
-                    }
-                }
-            `}
-        >
+        <Layout>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
@@ -58,7 +25,7 @@ function App() {
                     Learn React
                 </a>
             </header>
-        </div>
+        </Layout>
     )
 }
 
