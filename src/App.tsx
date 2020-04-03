ba8463afaa063330ca71ba7fc11fb5a24af41f30
Miikis <Miikis@users.noreply.github.com>
2020-04-02 17:35:11 -0700

    Add base & reset styles
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-import logo from "./logo.svg"
-
 import Layout from "./components/layout"
 
 function App() {
@@ -11,20 +9,6 @@ function App() {
             <Helmet>
                 <title>Home</title>
             </Helmet>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
         </Layout>
     )
 }
